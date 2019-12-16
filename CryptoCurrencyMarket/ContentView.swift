9d7b07bdb11673bb79e54d12a542ee6c7c41b9e7
YILING WU <ylwulibrarian@gmail.com>
2019-12-16 17:13:18 +0900

    Initial Commit

---
+++ CryptoCurrencyMarket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoCurrencyMarket
+//
+//  Created by Yi-Ling Wu on 2019/12/16.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
