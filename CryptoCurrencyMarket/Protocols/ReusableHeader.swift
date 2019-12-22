9378cdc7a26c37ddae187f9ae541b54ee8937274
YILING WU <ylwulibrarian@gmail.com>
2019-12-23 01:12:33 +0900

    [Storyboard] add selection view

--- CryptoCurrencyMarket/Protocols/ReusableHeader.swift
+++ CryptoCurrencyMarket/Protocols/ReusableHeader.swift
@@ -11,6 +11,7 @@ import UIKit
 class ReusableHeader: UIView {
 
     @IBOutlet weak var title: UIView!
+    var selection: UIView?
     
     let nibName = "ReusableHeader"
     
@@ -28,10 +29,29 @@ class ReusableHeader: UIView {
         guard let view = loadViewFromNib() else { return }
         view.frame = self.bounds
         self.addSubview(view)
+        setupView()
+    }
+    
+    func setupView() {
+        selection = UIView(frame: CGRect(x: 0, y: 80, width: self.frame.width, height: 0))
+        selection?.alpha = 0
+        selection?.layer.cornerRadius = 8.0
+        selection?.backgroundColor = UIColor.yellow
+        if let selection = selection { self.addSubview(selection) }
     }
     
     func loadViewFromNib() -> UIView? {
         let nib = UINib(nibName: nibName, bundle: nil)
         return nib.instantiate(withOwner: self, options: nil).first as? UIView
     }
+    
+    @IBAction func willChangeCurrencyPair(_ sender: Any) {
+        UIView.animate(withDuration: 0.1, delay: 0.1, options: [.curveEaseIn], animations: { [weak self] in
+            if let _self = self, let selection = _self.selection {
+                selection.alpha = 1
+                selection.frame = CGRect(x: 0, y: 80, width: _self.frame.width, height: 150)
+            }
+        }, completion: nil)
+    }
+    
 }
